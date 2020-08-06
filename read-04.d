3925a173ff4857540518e28100f9243b20201266
internetcharles <charlesodellsmith@gmail.com>
2020-08-06 08:32:37 -0700

    new notes

---
+++ read-04.d
@@ -0,0 +1,29 @@
+- URLSearchParams allows us to manipulate the query string of a URL.
+
+- URLSearchParams.append() - Appends a specified key/value pair as a new search parameter. 
+- URLSearchParams.delete() - Deletes the given search parameter, and its associated value, from the list of all search parameters. 
+- URLSearchParams.entries() - Returns an iterator allowing iteration through all key/value pairs contained in this object. 
+- URLSearchParams.forEach() - Allows iteration through all values contained in this object via a callback function. 
+- URLSearchParams.get() - Returns the first value associated with the given search parameter. 
+- URLSearchParams.getAll() - Returns all the values associated with a given search parameter. 
+- URLSearchParams.has() - Returns a Boolean indicating if such a given parameter exists. 
+- URLSearchParams.keys() - Returns an iterator allowing iteration through all keys of the key/value pairs contained in this object. URLSearchParams.set() - Sets the value associated with a given search parameter to the given value. If there are several values, the others are deleted. 
+- URLSearchParams.sort() - Sorts all key/value pairs, if any, by their keys. 
+- URLSearchParams.toString() - Returns a string containing a query string suitable for use in a 
+URL. 
+- URLSearchParams.values() - Returns an iterator allowing iteration through all values of the key/value pairs contained in this object.
+
+
+- Regex is useful in extracting info from text by searching for matches in a search pattern.
+- Works in validation, parsing/replacing strings, passing thru translating data, and web scraping.
+- The syntax is the same in almost all programming languages.
+
+
+- Asynchronous means things can happen independently of the main program flow.
+- Javascript is synchronous by default
+- JS uses the browser for asynchronicity
+- A callback is a function that’s passed to another function.
+- Callbacks add layers of nesting and complexity.
+- A promise is a proxy for a value that will eventually become available.
+- A promise is called in “pending” state
+
